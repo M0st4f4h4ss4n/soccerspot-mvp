ad706669a578c75b915a0654be962bcaafe28407
Mostafa Hassan <m0st4f4h4ss4n@odak-eg.com>
2023-06-03 02:41:14 +0300

    ok

--- src/logger.js
+++ src/logger.js
@@ -1,19 +1,33 @@
-const border = '==============================';
-const shade = '------------------------------';
+const border = '┌───────────────────────────────────────┐';
+const shade = '│                                       │';
 const reset = '\x1b[0m';
 
-function info(message, data = null) {
-  const emoji = '✅';
-  const color = '\x1b[32m';
-
-  console.log(`${color}${border}\n${shade}\n${emoji} ${message}\n${data ? `${shade}\n${JSON.stringify(data, null, 2)}\n` : ''}${border}${reset}`);
+function getCurrentTimestamp() {
+  const timestamp = new Date().toLocaleString('en-US', { timeZone: 'UTC' });
+  return timestamp;
 }
 
-function error(message, data = null) {
-  const emoji = '❌';
-  const color = '\x1b[31m';
+function logWithEmoji(icon, message, data = null, category = null) {
+  const colors = {
+    info: '\x1b[36m',
+    error: '\x1b[31m',
+    category: '\x1b[35m',
+    data: '\x1b[33m',
+  };
+  const timestamp = getCurrentTimestamp();
+  console.log(`${colors.info}${border}\n${shade}\n${icon} ${message}\n${data ? `${shade}\n${colors.data}Data: ${JSON.stringify(data, null, 2)}\n` : ''}${category ? `${shade}\n${colors.category}Category: ${category}\n` : ''}${shade}\n${icon} Time: ${timestamp}\n${border}${reset}`);
+}
 
-  console.log(`${color}${border}\n${shade}\n${emoji} ${message}\n${data ? `${shade}\n${JSON.stringify(data, null, 2)}\n` : ''}${border}${reset}`);
+function info(action, message, data = null, category = null) {
+  const emoji = 'ℹ️';
+  const infoMessage = `${action}: ${message}`;
+  logWithEmoji(emoji, infoMessage, data, category);
+}
+
+function error(action, message, data = null, category = null) {
+  const emoji = '🚨';
+  const errorMessage = `${action}: ${message}`;
+  logWithEmoji(emoji, errorMessage, data, category);
 }
 
 module.exports = { info, error };
